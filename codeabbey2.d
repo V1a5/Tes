1b884ffe53a6ba7fe4dc61575af747c619586ab0
V1a5 <vlasmamokiak@gmail.com>
2023-12-14 11:03:38 +0700

    tst1

---
+++ codeabbey2.d
@@ -0,0 +1,42 @@
+import std.stdio;
+
+void main()
+{
+    int n;
+    readf(" %d", &n);
+
+    int[] a = new int[](n);
+    for (int i = 0; i < n; ++i)
+    {
+        readf(" %d", &a[i]);
+    }
+
+    int s = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        s += a[i];
+    }
+
+    writeln(s);
+}
+
+// import std.stdio, std.algorithm, std.conv, std.array, std.math, std.string, std.range;
+// void main(){
+//     readln;
+//     writeln(sum(readln.split.map!(to!int [n]).array));
+// }
+
+// import std.stdio;
+// import std.algorithm;
+// import std.array;
+// import std.conv;
+// void main()
+// {
+//     int n;
+//     readf(" %d\n", &n);
+
+//     int[] a = readln.split.map!(to!int).array;
+    
+//     writeln(sum(a[0..n]));
+// }
+
